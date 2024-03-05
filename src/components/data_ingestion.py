5730c48005a807d6d4df7fab8e88ecd8cb48a803
Rishi Ranajan <101rishidsr@gmail.com>
2024-03-06 01:47:10 +0530

    model_trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,3 +1,14 @@
+'''
+a file named data_ingestion.py is likely used for data ingestion tasks.
+
+Data ingestion refers to the process of collecting, importing,
+ and processing data from various sources into a system or application for further analysis,
+   storage, or manipulation. 
+'''
+
+
+
+
 import os
 import sys
 from src.exception import CustomException
@@ -7,11 +18,11 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
-# from src.components.data_transformation import DataTransformation
-# from src.components.data_transformation import DataTransformationConfig
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
-# from src.components.model_trainer import ModelTrainerConfig
-# from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 @dataclass
          #using @dataclass we can easily define the class variable
 class DataIngestionConfig:
@@ -59,12 +70,12 @@ if __name__=="__main__":
     obj.initiate_data_ingestion()
 
 
-    # train_data,test_data=obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
 
-    # data_transformation=DataTransformation()
-    # train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
 
-    # modeltrainer=ModelTrainer()
-    # print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
 
 
